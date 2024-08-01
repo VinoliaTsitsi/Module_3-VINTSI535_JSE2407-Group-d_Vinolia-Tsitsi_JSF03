15d214e905be36419f498272fd09557a28bf63f5
VinoliaTsitsi <tsitsivinolia07@gmail.com>
2024-08-01 15:35:02 +0200

    installed the vue pakages. Created a header component with a logo and a navigation bar and listed items. exported the header and also added some styling onto the header component.

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+    <div claa="logo ">My App Logo</div>
+    <nav>
+        <li>categories</li>
+        <li>search</li>
+        <li>Login</li>
+        <li>cart</li>
+    </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+header {
+    background-color: #333;
+    color: #fff;
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+</style>
