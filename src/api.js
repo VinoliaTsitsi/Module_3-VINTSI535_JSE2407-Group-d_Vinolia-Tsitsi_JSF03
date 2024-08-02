7487fe28bfa371b1f6ea66ac745fe59709506d36
VinoliaTsitsi <tsitsivinolia07@gmail.com>
2024-08-02 11:37:49 +0200

    Added fetchProducts function to api.js to retrieve products from Fake Store API

--- src/api.js
+++ src/api.js
@@ -5,3 +5,12 @@ import axios from 'axios';
 *@returns {Promise<Object[]}
 
 */
+export const fetchProducts = async () => {
+  try {
+    const response = await axios.get('https://fakestoreapi.com/products');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    throw error;
+  }
+};
